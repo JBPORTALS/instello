6d5c74de69c4bfc84f5f5b8a6d2dcf5461c237fd
x-sss-x <manu48617@gmail.com>
2025-07-15 16:24:20 +0530

    Enhance Organization Handling in Dashboard Page
    - Integrated useOrganizationList to manage user memberships and set the active organization based on user data.
    - Added logic to automatically set the first organization as active if no active organization is present, improving user experience and navigation.

--- apps/dashboard/src/app/(dashboard)/page.tsx
+++ apps/dashboard/src/app/(dashboard)/page.tsx
@@ -2,16 +2,29 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import { useOrganization } from "@clerk/nextjs";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { SpinnerGapIcon } from "@phosphor-icons/react";
 
 export default function Page() {
   const { organization: activeOrganization } = useOrganization();
+  const { userMemberships, setActive } = useOrganizationList({
+    userMemberships: true,
+  });
 
   const router = useRouter();
   React.useEffect(() => {
     if (activeOrganization) router.replace(`/${activeOrganization.slug}`);
-  }, [activeOrganization, router]);
+    else {
+      const firstOrganization = userMemberships.data?.at(0);
+      if (firstOrganization) {
+        setActive?.({
+          organization: firstOrganization.organization,
+        })
+          .then()
+          .catch(() => console.log("Unable to set active organization"));
+      }
+    }
+  }, [activeOrganization, router, setActive, userMemberships.data]);
 
   return (
     <div className="flex h-svh w-full items-center justify-center">

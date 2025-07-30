e2a3a8c4e85ea6352483be9aa8460bd2191e2187
x-sss-x <manu48617@gmail.com>
2025-07-30 09:53:19 +0530

    Refactor TimeTable story to accept dynamic slots and args
    - Modified TimeTableStory to utilize props for slots and other parameters, enhancing flexibility.
    - Updated WithSlots story to include additional slot data and properties for better demonstration of the TimeTable component.

--- packages/ui/src/stories/TimeTable.stories.tsx
+++ packages/ui/src/stories/TimeTable.stories.tsx
@@ -18,32 +18,44 @@ export const Default: StoryObj = {
   render: () => <TimeTable editable />,
 };
 
-function TimeTableStory() {
-  const [slots, setSlots] = React.useState([
-    {
-      id: "1",
-      dayOfWeek: 1,
-      startOfPeriod: 1,
-      endOfPeriod: 3,
-      subject: "Math",
-    },
-    {
-      id: "2",
-      dayOfWeek: 2,
-      startOfPeriod: 2,
-      endOfPeriod: 4,
-      subject: "Physics",
-    },
-  ]);
+function TimeTableStory(args: Partial<React.ComponentProps<typeof TimeTable>>) {
+  const [slots, setSlots] = React.useState(args.slots);
 
   return (
     <>
       {/* <pre>{JSON.stringify(slots, undefined, 1)}</pre> */}
-      <TimeTable editable slots={slots} onChangeSlots={setSlots} />
+      <TimeTable {...args} slots={slots} onChangeSlots={setSlots} />
     </>
   );
 }
 
-export const WithSlots: StoryObj = {
-  render: () => <TimeTableStory />,
+export const WithSlots: StoryObj<React.ComponentProps<typeof TimeTable>> = {
+  args: {
+    slots: [
+      {
+        id: "1",
+        dayOfWeek: 1,
+        startOfPeriod: 1,
+        endOfPeriod: 3,
+        subject: "Math",
+      },
+      {
+        id: "2",
+        dayOfWeek: 2,
+        startOfPeriod: 2,
+        endOfPeriod: 4,
+        subject: "Physics",
+      },
+      {
+        id: "3",
+        dayOfWeek: 3,
+        startOfPeriod: 2,
+        endOfPeriod: 4,
+        subject: "Science",
+      },
+    ],
+    editable: true,
+    numberOfHours: 7,
+  },
+  render: TimeTableStory,
 };

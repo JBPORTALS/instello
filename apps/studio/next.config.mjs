4a3e4c33bacb4f4483e0c73a7066e38e3d7efe04
x-sss-x <manu48617@gmail.com>
2025-08-25 12:09:59 +0530

    Add new studio app for LMS app's dashboard
    - Upgraded Next.js to version 15.5.0, along with its peer dependencies including React and React DOM.
    
    - Added new dependencies for the studio app, including @instello/ui and various TypeScript configurations.
    - Updated existing dependencies to ensure compatibility with the latest Next.js version, enhancing overall project stability and performance.

---
+++ apps/studio/next.config.mjs
@@ -0,0 +1,22 @@
+import { createJiti } from "jiti";
+
+const jiti = createJiti(import.meta.url);
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+await jiti.import("./src/env");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@instello/ui"],
+  experimental: {
+    optimizePackageImports: ["@phosphor-icons/react"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;

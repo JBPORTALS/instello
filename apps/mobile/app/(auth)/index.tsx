cce0208b7cf596c90bc1ab1ad8f93ad4012f003e
x-sss-x <manu48617@gmail.com>
2025-09-19 20:10:03 +0530

    Update dependencies and enhance mobile app structure
    - Upgraded TypeScript and React versions to improve compatibility and performance.
    - Refactored mobile app layout to integrate new authentication features with ClerkProvider.
    - Added support for secure storage and improved theme management.
    - Updated package.json and pnpm-lock.yaml to reflect new dependencies and versions.
    - Removed unused assets and components to streamline the project structure.

---
+++ apps/mobile/app/(auth)/index.tsx
@@ -0,0 +1,51 @@
+import { View } from "react-native";
+import { Image } from "expo-image";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+
+export default function GetStarted() {
+  return (
+    <View className="flex-1 items-center justify-between gap-6 px-6 py-8">
+      <Image
+        source={require("assets/images/instello.png")}
+        style={{ height: 80, width: 140 }}
+        contentFit="contain"
+        transition={200}
+        useAppleWebpCodec
+        importantForAccessibility="yes"
+      />
+
+      <View className="bg-accent/20 border-border size-32 items-center justify-center rounded-2xl border shadow-lg">
+        <Image
+          source={require("assets/images/instello-feather.png")}
+          style={{ height: 100, width: 100 }}
+          contentFit="contain"
+          transition={200}
+          useAppleWebpCodec
+          importantForAccessibility="yes"
+        />
+      </View>
+
+      <View className="gap-3.5">
+        <Text variant={"h1"} className="max-w-sm text-center font-medium">
+          One Platform. Every Possibility.
+        </Text>
+        <Text variant={"lead"} className="text-center">
+          Learn anywhere. Teach better. Manage with ease.
+        </Text>
+      </View>
+
+      <View className="w-full gap-3.5">
+        <Text variant={"muted"} className="text-center">
+          by continuing you'll accept all our terms and conditions
+        </Text>
+        <Button className="w-full" variant={"secondary"} size={"lg"}>
+          <Text>Sign in</Text>
+        </Button>
+        <Button className="w-full" size={"lg"}>
+          <Text>Get Started</Text>
+        </Button>
+      </View>
+    </View>
+  );
+}
---
+++ apps/mobile/app/(onboarding)/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "@/components/ui/text";
+
+export default function Onboarding() {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  );
+}

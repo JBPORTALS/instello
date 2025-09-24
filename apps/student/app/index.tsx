47e2105538f7a510ddfbbdcc05334866df06133f
x-sss-x <manu48617@gmail.com>
2025-09-24 09:16:02 +0530

    Initialize the studet expo app with rnr components

---
+++ apps/student/app/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Stack } from "expo-router";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { useAuth } from "@clerk/clerk-expo";
+
+export default function Screen() {
+  const { signOut } = useAuth();
+  return (
+    <>
+      <Stack.Screen />
+      <View className="flex-1 items-center justify-center gap-8 p-4">
+        <Text>Welcome to Instello</Text>
+        <Button onPress={() => signOut()}>
+          <Text>Sign out</Text>
+        </Button>
+      </View>
+    </>
+  );
+}

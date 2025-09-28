fa9385acd02229f8c348f00e22f64d2639f12a39
x-sss-x <manu48617@gmail.com>
2025-09-28 19:38:50 +0530

    Refactor layout and remove unused pages for improved structure
    - Simplified the layout component by removing unnecessary elements such as the site header and channel details section, focusing on rendering only the children.
    - Deleted unused page components for chapters, coupons, and subscriptions to streamline the codebase and enhance maintainability.
    - Updated the video layout to only prefetch video data based on the video ID, improving data handling efficiency.

---
+++ apps/studio/src/app/(dashboard)/c/[channelId]/(channel)/layout.tsx
@@ -0,0 +1,25 @@
+import { ChannelPageBreadcrumb } from "@/components/channel-breadcrumb";
+import { ChannelDetailsSection } from "@/components/channel-details-section";
+import Container from "@/components/container";
+import { SiteHeader } from "@/components/site-header";
+import { HydrateClient } from "@/trpc/server";
+
+export default function ChannelLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <HydrateClient>
+      <SiteHeader startElement={<ChannelPageBreadcrumb />} />
+      <Container className="px-16">
+        <div className="grid grid-cols-10 gap-8">
+          {children}
+          <div className="col-span-3 h-full">
+            <ChannelDetailsSection />
+          </div>
+        </div>
+      </Container>
+    </HydrateClient>
+  );
+}
--- apps/studio/src/app/(dashboard)/c/[channelId]/layout.tsx
+++ apps/studio/src/app/(dashboard)/c/[channelId]/layout.tsx
@@ -1,7 +1,3 @@
-import { ChannelPageBreadcrumb } from "@/components/channel-breadcrumb";
-import { ChannelDetailsSection } from "@/components/channel-details-section";
-import Container from "@/components/container";
-import { SiteHeader } from "@/components/site-header";
 import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 
 export default async function Layout({
@@ -14,17 +10,5 @@ export default async function Layout({
   const { channelId } = await params;
   prefetch(trpc.lms.channel.getById.queryOptions({ channelId }));
 
-  return (
-    <HydrateClient>
-      <SiteHeader startElement={<ChannelPageBreadcrumb />} />
-      <Container className="px-16">
-        <div className="grid grid-cols-10 gap-8">
-          {children}
-          <div className="col-span-3 h-full">
-            <ChannelDetailsSection />
-          </div>
-        </div>
-      </Container>
-    </HydrateClient>
-  );
+  return <HydrateClient>{children}</HydrateClient>;
 }
